d3d6fd312e14341111511d897921db05364f73eb
Luong Anh Minh <anhminhluong2006@gmail.com>
2026-06-21 18:32:42 +0700

    add full data faq to chromadb

--- personal_agent/fun.py
+++ personal_agent/fun.py
@@ -1,23 +1,19 @@
 
 
 
-# from knowledge_base.vector_store import VectorStore
-# # from knowledge_base.embed import Embedder
+from knowledge_base.vector_store import VectorStore
+from knowledge_base.embed import Embedder
 # # from knowledge_base.loader import DataLoader
-# from knowledge_base.indexer import Indexer
+from knowledge_base.indexer import Indexer
 
-# indexer = Indexer(data_dir="data/raw/faq_bank")
-# result = indexer.run()
-# print(result)
-# vector_store = VectorStore()
-# print(vector_store.count())
+vector_store = VectorStore()
+query = "What are the premium payment frequencies available under this product ? "
+embedder = Embedder()
+embedding = embedder.embed(query)
+result = vector_store.query(query_embedding=embedding, n_results=2)
+print(result)
 
-from tools.branch_tool import BranchSearchTool,geocode
-branch_tool = BranchSearchTool()
-kwargs = {
-    "location": "kí túc xá kinh tế quốc dân hà nội",
-    "top_k": 1
-}
-context = branch_tool.safe_run(**kwargs)
-print(context.context)
+
+
+
 
